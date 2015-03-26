675b6d4e9573dde8257140c0f013af64eadb9ef3
faheem <faheem@3701d6cb-b936-491a-8507-f2d4cb0c2d3f>
2015-03-26 03:03:51 +0000

    Adding archetype from VS templates

---
+++ wnlb/trunk/WNLB/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace WNLB.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
