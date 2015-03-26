2a6112ceab55d3f7dd8627792c10633ef5d5317a
faheem <faheem@3701d6cb-b936-491a-8507-f2d4cb0c2d3f>
2015-03-26 03:03:51 +0000

    Adding archetype from VS templates

---
+++ WNLB/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WNLB.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ WNLB/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WNLB.WebApiApplication" Language="C#" %>
