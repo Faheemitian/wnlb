87dae63ec1ede92a60d9f9f727935a83f4af1b17
faheem <faheem@3701d6cb-b936-491a-8507-f2d4cb0c2d3f>
2015-05-07 21:30:50 +0000

    Adding basic load balancing module structure

---
+++ wnlb/trunk/WNLB/Modules/Application.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WNLB.Modules
+{
+    interface Application
+    {
+        public String AppName { get; set; }
+        public String AppPath { get; set; }
+        public List<AppServer> AppServers { get; set; }
+        public RequestRouter RequestRouter { get; set; }
+    }
+}
