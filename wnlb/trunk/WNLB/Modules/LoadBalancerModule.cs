647fdd4f40db4909091ebcdaac3404bfc961d3ea
faheem <faheem@3701d6cb-b936-491a-8507-f2d4cb0c2d3f>
2015-05-07 01:09:25 +0000

    Trial commit

---
+++ wnlb/trunk/WNLB/Modules/LoadBalancerModule.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WNLB.Modules
+{
+    public class LoadBalancerModule : IHttpModule
+    {
+        public LoadBalancerModule()
+        {
+        }
+
+        public void Init(HttpApplication context)
+        {
+            context.BeginRequest += new EventHandler(this.beginRequestHandler);
+            context.EndRequest += new EventHandler(this.endRequestHandler);
+        }
+
+        private void endRequestHandler(object sender, EventArgs e)
+        {
+            HttpApplication application = (HttpApplication)sender;
+            HttpContext context = application.Context;
+            HttpResponse response = context.Response;
+
+            response.Headers.Add("WNLB Handler", "Request Ended");
+        }
+
+        private void beginRequestHandler(object sender, EventArgs e)
+        {
+            HttpApplication application = (HttpApplication)sender;
+            HttpContext context = application.Context;
+            HttpResponse response = context.Response;
+
+            response.Headers.Add("WNL Handler", "Request Started");
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
