bf8a7a0986b618d5ecf13fdc6b963711cd664dde
faheem <faheem@3701d6cb-b936-491a-8507-f2d4cb0c2d3f>
2015-05-14 19:33:52 +0000

    Moving lib initialization code to WNLB project

---
+++ NLBLib/Servers/LocalAppServer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NLBLib.Servers
+{
+    public class LocalAppServer : AppServer
+    {
+        public LocalAppServer(string serverName)
+            : base("WNLB_Console", "localhost", 0)
+        {
+        }        
+    }
+}
