37d58525293fc5a78b58eecf7bca20199e8833b6
faheem <faheem@3701d6cb-b936-491a-8507-f2d4cb0c2d3f>
2015-05-14 21:32:53 +0000

    Added Unity IoC dependency and codes

---
+++ WNLB/Misc/INLBService.cs
@@ -0,0 +1,15 @@
+﻿using NLBLib.Applications;
+using NLBLib.Servers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WNLB.Misc
+{
+    public interface INLBService
+    {
+        ApplicationRegister AppRegister { get; set; }
+        ServerRegister ServerRegister { get; set; }
+    }
+}
