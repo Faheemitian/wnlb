bf94d48190f67070c467e537a04e20e3d916f247
faheem <faheem@3701d6cb-b936-491a-8507-f2d4cb0c2d3f>
2015-05-18 03:00:18 +0000

    Replace old vs web api template with mvc 4 web application and simple identity

--- WNLB/Controllers/HomeController.cs
+++ WNLB/Controllers/HomeController.cs
@@ -15,9 +15,23 @@ namespace WNLB.Controllers
 
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
 
             return View(NLBService.ServerRegister.GetServerWithName("Srv8003"));
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
         }
     }
 }
--- WNLB/Global.asax.cs
+++ WNLB/Global.asax.cs
@@ -15,15 +15,20 @@ using WNLB.Misc;
 
 namespace WNLB
 {
-    public class WebApiApplication : System.Web.HttpApplication
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-            GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            AuthConfig.RegisterAuth();
 
             StartLoadBalancerModule();
         }
