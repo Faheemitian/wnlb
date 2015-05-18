a500fde2018a42a5f078b5290523b66c41e44c81
faheem <faheem@3701d6cb-b936-491a-8507-f2d4cb0c2d3f>
2015-05-18 21:42:05 +0000

    Fixed database initialization code

---
+++ wnlb/trunk/WNLB/Misc/InitSecurityDb.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using WebMatrix.WebData;
+using WNLB.Models;
+
+namespace WNLB.Misc
+{
+    public class InitSecurityDb : DropCreateDatabaseIfModelChanges<UsersContext>
+    {
+        protected override void Seed(UsersContext context)
+        {
+
+            if (!WebSecurity.Initialized)
+            {
+                WebSecurity.InitializeDatabaseConnection("DefaultConnection", "UserProfile", "UserId", "UserName", autoCreateTables: true);
+            }
+            
+            var membership = (SimpleMembershipProvider)Membership.Provider;
+
+            if (membership.GetUser("admin", false) == null)
+            {
+                membership.CreateUserAndAccount("admin", "admin");
+            }
+        }
+    }
+}
